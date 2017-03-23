39f7f7284627bec4d349328f0cd5076680aad6f2
Gustavo Lazarotto Schroeder <guto_schroeder@hotmail.com>
2017-03-22 22:24:38 -0300

    Adicionado Scripts + Index.html
    verificar o modalContent pois nao sei o q faz :(

---
+++ app/script/controller.js
@@ -0,0 +1,77 @@
+angular.module('mwl.calendar.docs', ['mwl.calendar', 'ngAnimate', 'ui.bootstrap', 'colorpicker.module']);
+angular
+  .module('mwl.calendar.docs') //you will need to declare your module with the dependencies ['mwl.calendar', 'ui.bootstrap', 'ngAnimate']
+  .controller('angularController', function(moment, alert, calendarConfig) {
+    var vm = this;
+    //Para o calendário funcionar, estas variáveis devem ser setadas ao mínimo
+    vm.calendarView = 'month';
+	
+    vm.viewDate = new Date();
+    var actions = [{
+      label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
+      onClick: function(args) {
+        alert.show('Editado', args.calendarEvent);
+      }
+    }, {
+      label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+      onClick: function(args) {
+        alert.show('Deletado', args.calendarEvent);
+      }
+    }];
+	
+    vm.cellIsOpen = true;
+
+	vm.show = false;
+		
+    vm.addEvent = function() {
+      vm.events.push({
+        title: 'Digite aqui o nome do seu evento',
+        startsAt: moment().startOf('day').toDate(),
+        endsAt: moment().endOf('day').toDate(),
+        color: calendarConfig.colorTypes.important,
+        draggable: true,
+        resizable: true
+      });
+	  vm.show = true;
+    };
+
+    vm.eventClicked = function(event) {
+      alert.show('Clique', event);
+    };
+
+    vm.eventEdited = function(event) {
+      alert.show('Editado', event);
+    };
+
+    vm.eventDeleted = function(event) {
+      alert.show('Deletado', event);
+    };
+
+    vm.eventTimesChanged = function(event) {
+      alert.show('Dropped or resized', event);
+    };
+
+    vm.toggle = function($event, field, event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      event[field] = !event[field];
+    };
+
+    vm.timespanClicked = function(date, cell) {
+      if (vm.calendarView === 'month') {
+        if ((vm.cellIsOpen && moment(date).startOf('day').isSame(moment(vm.viewDate).startOf('day'))) || cell.events.length === 0 || !cell.inMonth) {
+          vm.cellIsOpen = false;
+        } else {
+          vm.cellIsOpen = true;
+          vm.viewDate = date;
+        }
+      } else if (vm.calendarView === 'year') {
+        if ((vm.cellIsOpen && moment(date).startOf('month').isSame(moment(vm.viewDate).startOf('month'))) || cell.events.length === 0) {
+          vm.cellIsOpen = false;
+        } else {
+          vm.cellIsOpen = true;
+          vm.viewDate = date;
+        }
+      }
+    };
+  });
